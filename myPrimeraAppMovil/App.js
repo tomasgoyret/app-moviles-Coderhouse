d943e6647c1277443eb2bb5a7aca33e579fb1839
tomasgoyret <tomas.goyret@gmail.com>
2022-02-25 20:39:46 -0300

    config navigation

--- myPrimeraAppMovil/App.js
+++ myPrimeraAppMovil/App.js
@@ -1,15 +1,24 @@
 import List from './components/List';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Text } from 'react-native-web';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   const [loaded] = useFonts({
-    Raleway: require('./assets/static/Raleway-Regular.ttf'),
-    RalewayBold: require('./assets/static/Raleway-Bold.ttf')
+    Raleway: require('./assets/fonts/static/Raleway-Regular.ttf'),
+    RalewayBold: require('./assets/fonts/static/Raleway-Bold.ttf')
   })
-  if(!loaded) return AppLoading
+  if (!loaded) return <AppLoading />
   return (
-    <List/>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name='List' component={List}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
