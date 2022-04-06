953fea1a9eb0fc6fe52dff1959d01c8fabb2596a
tomasgoyret <tomas.goyret@gmail.com>
2022-04-06 11:19:38 -0300

    nueva fuente

--- myPrimeraAppMovil/App.js
+++ myPrimeraAppMovil/App.js
@@ -14,7 +14,7 @@ init()
   })
 export default function App() {
   const [loaded] = useFonts({
-    Raleway: require('./assets/fonts/static/Raleway-Light.ttf'),
+    Raleway: require('./assets/fonts/static/Raleway-Medium.ttf'),
     RalewayBold: require('./assets/fonts/static/Raleway-Bold.ttf'),
     RalewayLightItalic: require('./assets/fonts/static/Raleway-LightItalic.ttf'),
   })
